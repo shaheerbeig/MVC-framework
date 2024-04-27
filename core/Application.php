73e3df2f78b127127181c842f83d1215c8cbb88a
shaheer beig <shaheerbeig@gmail.com>
2024-04-27 23:45:57 +0500

    implemented Application class responsible for handling creation of classes and calling the reolve function.

--- core/Application.php
+++ core/Application.php
@@ -2,15 +2,35 @@
 
 namespace app\core;
 use app\core\Router;
-
+use app\core\Request;
+use app\core\Statuscode;
 class Application{
 
-    protected Router $router;
-    public function __construct(){
-        $this->router = new Router();
+    public static Application $app;
+    public Router $router;
+    public Request $request;
+    public Statuscode $status;
+    public Statuscode $response;
+    public Database $db;
+    public static string $rootPath;
+    public $configuration = [
+        'db'=> [
+            'dsn' => $_ENV['DB_DSN'],
+            'user' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWORD']
+        ]
+    ];
+    public function __construct($rootDirectory){
+        $this->request = new Request();
+        $this->router = new Router($this->request);
+        $this->status = new Statuscode();
+        $this->response = new Statuscode();
+        $this->db = new Database($this->configuration['db']);
+        self::$app = $this;
+        self::$rootPath=$rootDirectory;
     }
 
     public function run(){
-        $this->router->get();
+        echo $this->router->resolve();
     }
 };
