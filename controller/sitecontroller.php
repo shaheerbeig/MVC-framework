06bcc08d36584e9d7855cee2fc1e023d3bf3c3e6
shaheer beig <shaheerbeig@gmail.com>
2024-05-09 00:17:03 +0500

    Sitecontroller is the main class that calls the contact and home pages.

---
+++ controller/sitecontroller.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\controller;
+use app\core\Application;
+use app\core\Controllers;
+use app\core\Request;
+use app\model\Contact;
+
+class Sitecontroller extends Controllers{
+    public function contact(Request $request){
+        Application::$app->title = "Contact";
+        $contact = new Contact();
+
+        if($request->getmethod() == 'post'){
+            $contact->LoadData($request->getBody());
+                if($contact->validateData() && $contact->send()){
+                    Application::$app->session->createFlash('success','Thank you for contacting Us. ','success');
+                    header('Location: /contact');
+                    exit();
+                }
+        }
+        return Application::$app->router->render('contact',[
+            'model' => $contact
+        ]);
+    }
+    public function home(){
+        Application::$app->title = "Home";
+        return Application::$app->router->render('home');
+    }
+};
