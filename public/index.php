162450dd2e0b290eae73730f8edab2ed99e224ba
shaheer beig <shaheerbeig@gmail.com>
2024-05-09 22:23:53 +0500

    main index.php file

--- public/index.php
+++ public/index.php
@@ -9,21 +9,25 @@ $dotenv->load();
 
 $rootDirectory = dirname(__DIR__);
 $configuration = [
+    'user' => app\model\users::class,
     'db'=> [
-        'dsn' => $_ENV['DB_DSN'],
+        'dbname' => $_ENV['DB_DATABASE'],
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
         'user' => $_ENV['DB_USER'],
         'password' => $_ENV['DB_PASSWORD']
     ]
 ];
-$app = new Application($rootDirectory,$configuration['db']);
+$app = new Application($rootDirectory,$configuration);
 
 $app->router->get('/',[Sitecontroller::class, 'home']);
 $app->router->get('/contact', [Sitecontroller::class, 'contact']);
-$app->router->post('/contact', [Sitecontroller::class, 'HomeControllerRender']);
+$app->router->post('/contact', [Sitecontroller::class, 'contact']);
 
 $app->router->get('/register', [Authcontroller::class, 'register']);
 $app->router->post('/register', [Authcontroller::class, 'register']);
 $app->router->get('/login', [Authcontroller::class, 'login']);
 $app->router->post('/login', [Authcontroller::class, 'login']);
-
+$app->router->get('/logout', [Authcontroller::class, 'logout']);
+$app->router->get('/profile', [Authcontroller::class, 'profile']);
 $app->run();
